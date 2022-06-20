63d2934af3cdbd5447b1b9d0fa0a01400298b327
ildar-icoosoft <ildar.icoosoft@gmail.com>
2022-06-21 02:51:19 +0500

    style: Стилизовал ноды (#3)

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import {Canvas} from "./canvas.js";
 import mockData from './data.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  let root = document.getElementById('diagram-container');
+  let root = document.getElementById('flowchart-container');
   const canvas = new Canvas({
     root
   });
